06eaac8d211bf4f6f8468924081186545596ebb0
Cinquin Andy <cinquin.andy@gmail.com>
2024-11-16 19:37:27 +0100

    feat(ui): little improved

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -30,41 +30,32 @@ export const Header = ({
 	}
 	return (
 		<div className="sticky top-0 z-50 rounded-b-2xl backdrop-blur-2xl">
-			<div className="relative rounded-b-2xl bg-gradient-to-b from-black/50 to-black/20 pb-6 pt-8 shadow-2xl shadow-white/10">
-				<div className={'absolute left-0 top-0 pt-2'}>
-					<Link href="/">
-						<Image
-							src="/images/logo.svg"
-							className={'invert'}
-							alt="League of Legends logo"
-							width={150}
-							height={150}
-						/>
-					</Link>
-				</div>
-				<div className="mx-auto max-w-7xl px-4">
-					<div className="flex flex-col gap-4 sm:flex-row sm:items-center">
-						<div className="relative flex-1">
-							<Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-slate-400" />
-							<Input
-								type="text"
-								placeholder="Search champions..."
-								value={searchTerm}
-								onChange={e => onSearch(e.target.value)}
-								className="w-full pl-10 pr-4"
-							/>
-							<ResetIcon
-								className="absolute right-3 top-1/2 h-4 w-4 -translate-y-1/2 cursor-pointer text-slate-400"
-								onClick={reset}
+			<div className="relative flex w-full items-center rounded-b-2xl bg-gradient-to-b from-black/50 to-black/20 pb-6 pt-8 shadow-2xl shadow-white/10">
+				<div className={'w-full'}>
+					<div className="mx-auto max-w-7xl px-4">
+						<div className="flex flex-col gap-4 sm:flex-row sm:items-center">
+							<div className="relative flex-1">
+								<Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-slate-400" />
+								<Input
+									type="text"
+									placeholder="Search champions..."
+									value={searchTerm}
+									onChange={e => onSearch(e.target.value)}
+									className="w-full pl-10 pr-4"
+								/>
+								<ResetIcon
+									className="absolute right-3 top-1/2 h-4 w-4 -translate-y-1/2 cursor-pointer text-slate-400"
+									onClick={reset}
+								/>
+							</div>
+
+							<SortDropdown
+								sortBy={sortBy}
+								sortDirection={sortDirection}
+								onSort={onSort}
+								onToggleDirection={onToggleDirection}
 							/>
 						</div>
-
-						<SortDropdown
-							sortBy={sortBy}
-							sortDirection={sortDirection}
-							onSort={onSort}
-							onToggleDirection={onToggleDirection}
-						/>
 					</div>
 				</div>
 			</div>
