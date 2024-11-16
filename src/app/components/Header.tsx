3faee78e176524c03e1fd093ce7c8425633d51f1
Cinquin Andy <cinquin.andy@gmail.com>
2024-11-16 17:51:59 +0100

    feat(reset): add reset function
    - Introduced a reset function to clear search and sort.
    - Added a reset icon that triggers the reset on click.
    - Enhanced user experience by allowing quick resets in the header.

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,6 +4,7 @@ import { Search } from 'lucide-react'
 import { Input } from '@/components/ui/input'
 
 import { SortDropdown } from '@/app/components/SortDropdown'
+import { ResetIcon } from '@radix-ui/react-icons'
 
 export const Header = ({
 	searchTerm,
@@ -20,6 +21,11 @@ export const Header = ({
 	onToggleDirection: () => void
 	sortDirection: 'asc' | 'desc'
 }) => {
+	const reset = () => {
+		onSearch('')
+		onSort('name')
+		sortDirection === 'desc' && onToggleDirection()
+	}
 	return (
 		<div className="sticky top-0 z-50 backdrop-blur-xl">
 			<div className="bg-gradient-to-b from-white/80 to-white/40 pb-6 pt-8">
@@ -44,6 +50,10 @@ export const Header = ({
 								value={searchTerm}
 								onChange={e => onSearch(e.target.value)}
 								className="w-full pl-10 pr-4"
+							/>
+							<ResetIcon
+								className="absolute right-3 top-1/2 h-4 w-4 -translate-y-1/2 cursor-pointer text-slate-400"
+								onClick={reset}
 							/>
 						</div>
 
